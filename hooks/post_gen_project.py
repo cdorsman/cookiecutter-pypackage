892340e8160cd7caa7951a1c067a63b245b036f0
Chris Dorsman <c.dorsman@gmail.com>
2022-02-03 22:00:17 +0100

    Customized #1

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python
 import os
+import shutil
 
 PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 
@@ -7,16 +8,20 @@ PROJECT_DIRECTORY = os.path.realpath(os.path.curdir)
 def remove_file(filepath):
     os.remove(os.path.join(PROJECT_DIRECTORY, filepath))
 
+def remove_directory(dirpath):
+    shutil.rmtree(os.path.join(PROJECT_DIRECTORY, dirpath))
 
 if __name__ == '__main__':
 
     if '{{ cookiecutter.create_author_file }}' != 'y':
         remove_file('AUTHORS.rst')
         remove_file('docs/authors.rst')
-
-    if 'no' in '{{ cookiecutter.command_line_interface|lower }}':
+    
+    if '{{ cookiecutter.command_line_interface }}' != 'y':
+        lib_dir = os.path.join('{{ cookiecutter.project_slug }}', 'lib')
         cli_file = os.path.join('{{ cookiecutter.project_slug }}', 'cli.py')
         remove_file(cli_file)
+        remove_directory(lib_dir)
 
     if 'Not open source' == '{{ cookiecutter.open_source_license }}':
         remove_file('LICENSE')
--- {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
+++ {{cookiecutter.project_slug}}/{{cookiecutter.project_slug}}/cli.py
@@ -1,35 +1,68 @@
+#!/usr/bin/env python3
 """Console script for {{cookiecutter.project_slug}}."""
 
-{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
-import argparse
-{%- endif %}
-import sys
-{%- if cookiecutter.command_line_interface|lower == 'click' %}
-import click
+from sys import exit
+from platform import python_version, machine, system
+from os import utime, makedirs
+from os.path import exists, dirname, abspath, join
+
+from logging import getLogger
+from logging.config import fileConfig
+
+{% if cookiecutter.command_line_interface == 'yes' %}
+from lib.parser import Parser
 {%- endif %}
 
-{% if cookiecutter.command_line_interface|lower == 'click' %}
-@click.command()
-def main(args=None):
-    """Console script for {{cookiecutter.project_slug}}."""
-    click.echo("Replace this message by putting your code into "
-               "{{cookiecutter.project_slug}}.cli.main")
-    click.echo("See click documentation at https://click.palletsprojects.com/")
-    return 0
+{%- if cookiecutter.config_setup == 'yes' %}
+from lib.config import config
 {%- endif %}
-{%- if cookiecutter.command_line_interface|lower == 'argparse' %}
+
 def main():
-    """Console script for {{cookiecutter.project_slug}}."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument('_', nargs='*')
-    args = parser.parse_args()
-
-    print("Arguments: " + str(args._))
-    print("Replace this message by putting your code into "
-          "{{cookiecutter.project_slug}}.cli.main")
-    return 0
-{%- endif %}
+    """ Main """
 
 
-if __name__ == "__main__":
-    sys.exit(main())  # pragma: no cover
+if __name__ == '__main__':
+    {%- if cookiecutter.command_line_interface == 'yes' %}
+    
+    parser = Parser()
+    cf = config(parser.config_file)
+    
+    try:
+        if exists(parser.default_logfile) is False:
+            basedir = dirname(parser.default_logfile)
+            if not exists(basedir):
+                makedirs(basedir)
+            with open(parser.default_logfile, 'a'):
+                utime(parser.default_logfile, None)
+    except OSError as oserr:
+        print('Cannot open file:', parser.default_logfile)
+        exit(1)
+    {%- endif %}
+
+    {%- if cookiecutter.command_line_interface == 'yes' %}
+    __loggername__ = parser.app_name.title()
+    fileConfig(fname=parser.default_logconf)
+    {%- else %}
+    __loggername__ = "{{cookiecutter.project_slug}}"
+    fileConfig(fname={}).format(
+        join(
+            dirname(dirname(abspath(__file__))),
+            'conf')
+    )
+    {%- endif %}
+    log = getLogger(__loggername__)
+    log.debug('%s-%s running on Python version: %s-%s on %s',
+        __loggername__,
+        '{{ cookiecutter.version }}',
+        python_version(),
+        machine(),
+        system())
+    log.debug('Configuration settings:\n\n \
+Approot: %s |\n \
+Logconfig file: %s |\n \
+Config file: %s |\n',
+                  parser.app_root,
+                  parser.default_logconf,
+                  parser.config_file)
+
+    main()
